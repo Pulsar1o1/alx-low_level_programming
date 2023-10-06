b970183b840a4d9885aa5f65a4d2b2b42e1e4bb8
root <Habibi.ElMostafa@proton.me>
2023-10-06 16:27:57 -0700

    print combo advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*A program that prints combination of all two digit numbers*/
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+
+			if (x < 8 || y < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+			if (x == 8 && y == 9)
+				putchar('\n');
+		}
+	}
+
+	return (0);
+}

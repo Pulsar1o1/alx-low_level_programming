0bc7ca42cea350e2cb64fda3ce171c67550fc948
root <Habibi.ElMostafa@proton.me>
2023-10-10 17:11:09 -0700

    updated 101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,8 +3,6 @@
 /**
  * main - program that computes and prints the sum of all the multiples of 3 or 5 below 1024
  *
- * Description: check for all natural numbers in range of
- * 3 and 5 and sum them
  * Return: 0
  */
 
